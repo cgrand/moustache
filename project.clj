d141a171d0a5dcf1d5cc72240affdce21297e150
Christophe Grand <cgrand@ks358968.kimsufi.com>
2010-03-29 23:44:29 +0200

    Refine project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "a micro web framework/internal DSL to wire Ring handlers and middlewares"
   :url "http://github.com/cgrand/moustache/"
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [ring/ring-core "0.2.0-RC2"]])
+                 [ring/ring-core "0.2.0"]]
+  :dev-dependencies [[ring "0.2.0"]])
