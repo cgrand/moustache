fcfdc3fa941171a552b93556305b9699a3a075ff
Christophe Grand <christophe@cgrand.net>
2010-03-29 18:23:06 +0200

    leinification

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject org.buntin/moustache "1.0.0-SNAPSHOT"
+  :description "a micro web framework/internal DSL to wire Ring handlers and middlewares"
+  :url "http://github.com/cgrand/moustache/"
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]]
+                 [ring/ring-core "0.2.0-RC2"]])
