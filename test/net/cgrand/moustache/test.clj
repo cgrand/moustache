e0ec7e6968e9623fb282e3140357d42b68abe668
Christophe Grand <christophe@cgrand.net>
2011-03-09 12:52:19 +0100

    tests migrated to clojure.test

--- test/net/cgrand/moustache/test.clj
+++ test/net/cgrand/moustache/test.clj
@@ -1,6 +1,6 @@
 (ns net.cgrand.moustache.test
   (:use net.cgrand.moustache)
-  (:use [clojure.contrib.test-is :as test-is :only [set-test is are]]))
+  (:use [clojure.test :only [deftest is are]]))
 
 
 (defn hello [_]
@@ -23,26 +23,25 @@
   (let [{:keys [status body]} (apply request handler uri options)]
     (when (= status 200) body))) 
 
-(set-test app
-  ;; wrapping
+(deftest wrapping
   (is (found+content (app hello) "/random/url"))
-  (is (found+content (app (fn [req] (hello req))) "/random/url"))
+  (is (found+content (app (fn [req] (hello req))) "/random/url")))
   
-  ;; response literal
+(deftest response-literal
   (is (= "literal response"
         (found+content (app {:status 200
-                             :body "literal response"}) "/random/url")))
+                             :body "literal response"}) "/random/url"))))
   
-  ;; fixed route dispatch      
+(deftest fixed-route-dispatch      
   (is (found+content (app [] hello) "/"))
   (is (not+found (app [] hello) "/random/url"))
   
   (is (found+content (app ["some" "url"] hello) "/some/url"))
   (is (not+found  (app ["some" "url"] hello) "/"))
   (is (not+found  (app ["some" "url"] hello) "/some/url/"))
-  (is (not+found  (app ["some" "url"] hello) "/some/url/foo"))
+  (is (not+found  (app ["some" "url"] hello) "/some/url/foo")))
   
-  ;; open route dispatch      
+(deftest open-route-dispatch      
   (is (found+content (app ["some" "url" &] hello) "/some/url"))
   (is (not+found  (app ["some" "url" &] hello) "/"))
   (is (found+content  (app ["some" "url" &] hello) "/some/url/"))
@@ -51,9 +50,9 @@
   (is (= "xs=0" (found+content (app ["some" "url" & xs] ["xs=" (count xs)]) "/some/url")))
   (is (= "xs=1" (found+content (app ["some" "url" & xs] ["xs=" (count xs)]) "/some/url/")))
   (is (= "xs=1" (found+content (app ["some" "url" & xs] ["xs=" (count xs)]) "/some/url/bar")))
-  (is (= "xs=2" (found+content (app ["some" "url" & xs] ["xs=" (count xs)]) "/some/url/bar/foo")))
+  (is (= "xs=2" (found+content (app ["some" "url" & xs] ["xs=" (count xs)]) "/some/url/bar/foo"))))
   
-  ;; segment validators
+(deftest segment-validators
   (is (= "url" (found+content (app ["some" a] [{:status 200 :body a}]) "/some/url")))
   (is (= "thing" (found+content (app ["some" a] [{:status 200 :body a}]) "/some/thing")))
   (is (not+found (app ["some" a] [{:status 200 :body a}]) "/any/url"))
@@ -70,26 +69,32 @@
   
   (is (= "x=01;y=04" (found+content (app [[[_ x y] #"(\d+)-(\d+)"]] ["x=" x ";y=" y]) "/01-04")))
   (is (found+content (app [[_ #(= 3 (count %))]] "ok") "/abc"))
-  (is (not+found (app [[_ #(= 3 (count %))]] "ok") "/abcd"))
+  (is (not+found (app [[_ #(= 3 (count %))]] "ok") "/abcd")))
   
-  ;; method dispatch
+(deftest method-dispatch
   (is (found+content (app :get "ok") "/" :request-method :get))
   (is (= {:status 405, :headers {"Allow" "GET"}} (request (app :get "ok") "/" :request-method :post)))
   (is (= {:status 405, :headers {"Allow" "GET"}} (request (app :get "ok") "/" :request-method :put)))
-  (is (= {:status 405, :headers {"Allow" "GET"}} (request (app :get "ok") "/" :request-method :head)))
+  (is (= {:status 405, :headers {"Allow" "GET"}} (request (app :get "ok") "/" :request-method :head))))
   
-  ;; fallthrough
+(deftest fallthrough
   (is (= "route1" (found+content (app ["foo" &] "route1" ["foo" "bar"] "route2") "/foo/bar")))
-  (is (= "route2" (found+content (app ["foo" &] pass ["foo" "bar"] "route2") "/foo/bar")))
+  (is (= "route2" (found+content (app ["foo" &] pass ["foo" "bar"] "route2") "/foo/bar"))))
   
-  ;; nested apps
+(deftest nested-apps
   (is (found+content (app ["foo" &] [["bar"] "ok"]) "/foo/bar"))
   (is (not+found (app ["foo" &] [["bar"] "ok"]) "/foo/baz"))
-  (is (not+found (app ["foo" &] [["bar"] "ok"]) "/foe/bar"))
+  (is (not+found (app ["foo" &] [["bar"] "ok"]) "/foe/bar")))
   
-  ;; middlewares
+(deftest middlewares
   (is (= {:status 200, :headers {"Content-Type" "text/html"}, :body "<h3>not text!</h3>"} 
         (request (app (alter-response assoc-in [:headers "Content-Type"] "text/html") "<h3>not text!</h3>") "/")))
   (is (found+content (app (alter-request update-in [:uri] #(str "/foo" %)) ["foo" "bar"] "ok") "/bar"))  
-  (is (not+found (app (alter-request update-in [:uri] #(str "/foo" %)) ["foo" "bar"] "ok") "/foo/bar"))  
-) 
+  (is (not+found (app (alter-request update-in [:uri] #(str "/foo" %)) ["foo" "bar"] "ok") "/foo/bar")))
+
+(deftest params
+  (is (= {:body "bar/bat%"}
+        ((app {:keys [foo biz]} {:body (str foo "/" biz)}) {:query-string "foo=bar&biz=bat%25"})))           
+  (is (= {:body "/"}
+        ((app {:keys [foo biz]} {:body (str foo "/" biz)}) {:query-string "foo=bar&biz=bat%25"
+                                                            :params {}}))))
